34d49a7413e7177de384d16a9e0eb3f5f253f63e
nikku18 <nirajvarma189@gmail.com>
2016-10-29 19:06:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool
+
+var config = {
+   host: 'db.imad.hasura-app.io',
+   user: 'nikku18',
+  port: '5432',
+  database: 'nikku18',
+  password: process.env.DB_PASSSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
 
+var pool = new Pool(config);
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  pool.query('SELECT * FROM test',function(err,req));
 });
 
 app.get('/ui/style.css', function (req, res) {
